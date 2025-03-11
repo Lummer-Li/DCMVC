a70643b5332d6af00ec152e0779abfef8add203c
Lummer <68311971+Lummer-Li@users.noreply.github.com>
2025-03-11 17:37:17 +0800

    Delete parse.py

--- parse.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='命令行中传入一个数字') #也可以直接()，不用description
-parser.add_argument('integers', type=str, help='传入的数字')
-
-args = parser.parse_args() #解析方法
-
-#获得传入的参数
-print(args)
